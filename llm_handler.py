fbbb53dbf317770ce2a07047e7ef8f58f490d2ed
aniruddh vyas <aniruddhzdvyas@gmail.com>
2025-03-05 02:11:19 +0100

    prompt changes

--- llm_handler.py
+++ llm_handler.py
@@ -4,7 +4,7 @@ from langchain_openai import ChatOpenAI  # Updated import
 
 class LLMHandler:
 
-    def __init__(self, api_key, model="gpt-4o", temperature=0.7):
+    def __init__(self, api_key, model="gpt-4o-mini", temperature=0.7):
         self.llm = ChatOpenAI(
             openai_api_key=api_key,
             model=model,
@@ -26,16 +26,16 @@ class LLMHandler:
     def analyze_vulnerability(self, context: str, question: str) -> str:
         """Perform vulnerability analysis using RAG"""
         prompt_template = ChatPromptTemplate.from_template(
-            """Analyse the given code snippet in question with the help of the relevant context
-            **Relevant Context**: {context}
-            From the relevant context, pay extra attention to the violated rules and 
-            accompanying messages to provide more accurate results
-            **Vulnerable Code**: {question}
+            """ You are Java Cryptography Architecture (JCA) developer and you are tasked with
+            analyzing a given code snippet and providing a secure alternate code snippet with modern standards.
+            Analyse the given code snippet: {question}
+            Relevant Context(includes cognicrypt violation report and rules the code violated): {context}
+            
+            Give you analysis and provide a secure code snippet in the following way. 
             Required Format: (Use Markdown)
             - Vulnerability Name: [Name]
-            - Correct Code Snippet: [Java]
-                [secure code]
-            - Technical Explanation: [120-150 words]
+            - Correct Code Snippet: give only a single line of code
+            - Explanation: [120-150 words]
             """
         )
         chain = prompt_template | self.llm | self.output_parser
